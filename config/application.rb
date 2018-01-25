58d8bca0bc4a52ee8e039b812e3b6f0091977bd1
Akash235 <pawarakash235@gmail.com>
2018-01-25 12:07:12 +0530

    to allow preflight request for angular app and also sending welcome email to the register user

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,14 @@ module WeatherApi
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # http://guides.rubyonrails.org/generators.html
+    # to allow preflight request in rails server
+
+    config.action_dispatch.default_headers.merge!({
+    'Access-Control-Allow-Origin' => '*',
+    'Access-Control-Request-Method' => '*'
+    })
+
+     # http://guides.rubyonrails.org/generators.html   
     config.generators do |g|
       g.test_framework  nil
       g.stylesheets     false
