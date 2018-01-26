d3d376fc976158df92e7ffc2fb3a35d6be62afa6
Akash235 <pawarakash235@gmail.com>
2018-01-26 20:45:32 +0530

    to chnage from api/v1/auth to api/v1/auth/token

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      post :auth, to: "authentication#create"
+      post :'auth/token', to: "authentication#create"
 
       resources :user_details do
         resources :recordings
